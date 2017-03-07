9fa71d3eafd1637165787c5b61ed0076c9dd02d1
Rieß, Lasse (Dozent) <lasse.riess@gmail.com>
2017-03-07 22:38:29 +0100

    truffle file modified

--- truffle.js
+++ truffle.js
@@ -3,7 +3,14 @@ module.exports = {
     development: {
       host: "localhost",
       port: 8545,
-      network_id: "*" // Match any network id
-    }
+      network_id: "1337" // Match any network id
+    },
+      live: {
+          host: "localhost",
+         port: 8545,
+          network_id: "default",
+          from: "7462b541d9bd67f33da72f910af3dde9b54fdea7"
+          
+      }
   }
 };
